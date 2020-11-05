37f57a84307b15e740773e5986219679d93c08c0
Imran Ahmad <imran@concernbox.com>
2020-11-06 05:01:39 +0530

    Deploy app to heroku
    - Deploy as staging env
    - Allow cors

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,17 @@ Bundler.require(*Rails.groups)
 
 module CoursePortal
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :head, :options]
+      end
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Mumbai'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
